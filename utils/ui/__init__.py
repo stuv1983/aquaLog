daacfad3b64eb6567a5c705eaa8a0837e4bd3b5a
stuv1983 <147810570+stuv1983@users.noreply.github.com>
2025-06-14 22:39:32 +1000

    Update __init__.py

--- utils/ui/__init__.py
+++ utils/ui/__init__.py
@@ -1,21 +1,15 @@
-'''utils/__init__.py: Aggregator for all utility modules'''  
-# Core helpers  
-from .core import is_mobile, get_viewport_width  
+# utils/ui/__init__.py
+"""
+UI subpackage initializer for AquaLog utilities
+"""
 
-# Chemistry calculations  
-from .chemistry import calc_unionized_ammonia, adjust_ph  
+# Re-export alerts functions
+from .alerts import request_rerun, show_toast, show_out_of_range_banner, show_parameter_advice
 
-# Database connection utilities  
-from .database import get_connection, execute_query  
+# Re-export charting functions
+from .charts import clean_numeric_df, rolling_summary, multi_param_line_chart
 
-# Localization and formatting  
-from .localization import translate, format_with_units  
-
-# Validation routines  
-from .validation import validate_ph, validate_nitrate, validate_ammonia  
-
-# UI utilities (alerts and rerun)  
-from .ui.alerts import request_rerun, show_toast, show_out_of_range_banner, show_parameter_advice  
-
-# Charting functions  
-from .ui.charts import clean_numeric_df, rolling_summary, multi_param_line_chart
+__all__ = [
+    "request_rerun", "show_toast", "show_out_of_range_banner", "show_parameter_advice",
+    "clean_numeric_df", "rolling_summary", "multi_param_line_chart",
+]
