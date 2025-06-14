ffd51948a5ec5d8b02a32afc6cc09c931c2664ab
Stu <stuart.villanti@gmail.com>
2025-06-14 13:29:55 +1000

    updated utils

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,17 +1,20 @@
 # utils/__init__.py
 
-from .core import cache_data
-from .validation import (
-    validate_parameter,
-    validate_range,
+from .core import (
+    is_mobile,
+    load_config,
+    cache_data,
+    clear_cache,
 )
-from .localization import (
-    translate,
-    format_with_units,
+from .validation import (
+    validate_numeric,
+    is_out_of_range,
 )
 from .chemistry import (
+    calculate_nh3_fraction,
     calculate_equilibrium_dose,
     calculate_alkaline_buffer_dose,
 )
-from . import database
-from .ui import alerts, charts
+from .charts import render_line_chart, render_correlation_heatmap
+from .alerts import show_out_of_range_banner
+from .localization import translate
