285fcb8dc70d850b3aa48d30cc8f82b127f44848
Stu <stuart.villanti@gmail.com>
2025-06-15 22:06:41 +1000

    base updated

--- aqualog_db/base.py
+++ aqualog_db/base.py
@@ -34,8 +34,8 @@ class BaseRepository:
             conn.row_factory = sqlite3.Row
             conn.execute("PRAGMA foreign_keys = ON;")
             
-            # FIX: Removed WAL pragma as it is unsupported in the deployment environment.
-            # conn.execute("PRAGMA journal_mode = WAL;")
+            # FIX: Removed WAL pragma as it causes a disk I/O error in the deployment environment.
+            # conn.execute("PRAGMA journal_mode = WAL;") 
             
             conn.execute("PRAGMA synchronous = NORMAL;")
             self._local.conn = conn
